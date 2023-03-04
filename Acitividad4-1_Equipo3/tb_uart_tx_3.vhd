16acde7085d763c05eebe73901298a9dc9252387
EmilianoHFlores <A00833160@tec.mx>
2023-03-04 09:44:06 -0600

    Update tb_uart_tx_3.vhd

--- Acitividad4-1_Equipo3/tb_uart_tx_3.vhd
+++ Acitividad4-1_Equipo3/tb_uart_tx_3.vhd
@@ -21,12 +21,14 @@ architecture Behave of tb_uart_tx_3 is
 	signal d_in_tb   	: std_logic_vector(7 downto 0)	:= (others => '0');
 	signal tx_done_tick_tb	: std_logic	;
 	signal tx_tb 		: std_logic	;
+	signal state_tb : std_logic_vector (1 downto 0);
 
 	component UART_TX_3 is
 
 	port(in_sw : in std_logic_vector(7 downto 0);
 		  tx_start,clk,rst: in std_logic;
-		  s_tx, tx_done_tick: out std_logic
+		  s_tx, tx_done_tick: out std_logic;
+		  state_out : out std_logic_vector(1 downto 0)
 		  );
 		  
 	end component UART_TX_3;	
@@ -36,13 +38,17 @@ begin
 	clk_tb <= not clk_tb after c_CLK_PERIOD/2;
 
    	-- Instantiate UART transmitter
-	DUT:	UART_TX_3 port map(d_in_tb, tx_start_tb, clk_tb, '0', tx_tb, tx_done_tick_tb);
+	DUT:	UART_TX_3 port map(d_in_tb, tx_start_tb, clk_tb, '0', tx_tb, tx_done_tick_tb, state_tb);
    
 	process is
 	begin
 		wait for 0 ns;
-		tx_start_tb   <= '1'; d_in_tb <= X"55"; 
-		wait for 20 ns;
-		tx_start_tb   <= '0';
+		tx_start_tb   <= '1'; d_in_tb <= "11001100";
+		wait for 20000 ns;
+		tx_start_tb   <= '0'; d_in_tb <= "11001100";
+		wait for 200000 ns;
+		tx_start_tb   <= '1'; d_in_tb <= "10101010";
+		wait for 20000 ns;
+		
 	end process;
 end Behave;
