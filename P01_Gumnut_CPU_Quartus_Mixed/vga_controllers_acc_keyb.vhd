8a1263b77fa0a68a267d257b58f695ce55d694f6
David Israel Vázquez Leal <david_vl2003@hotmail.com>
2023-03-14 18:59:24 -0600

    Implementacion de teclaod matricial

--- P01_Gumnut_CPU_Quartus_Mixed/vga_controllers_acc_keyb.vhd
+++ P01_Gumnut_CPU_Quartus_Mixed/vga_controllers_acc_keyb.vhd
@@ -6,6 +6,7 @@ ENTITY vga_controllers_acc_keyb IS
 	PORT (
 		CLOCK_50: IN STD_LOGIC; --50MHz in our board
 		SW : in STD_LOGIC_VECTOR (9 downto 0); --SWITCHES
+		LEDR : OUT STD_LOGIC_VECTOR (9 downto 0); --borrar
 		
 		--Screen inputs
 		pixel_clk: BUFFER STD_LOGIC;
@@ -19,7 +20,15 @@ ENTITY vga_controllers_acc_keyb IS
 		GSENSOR_SDO			: INOUT	std_logic;
 		GSENSOR_CS_N		: OUT		std_logic;
 		GSENSOR_SCLK		: OUT		std_logic;
+		
+		--Matricial inputs
+		col   : IN  STD_LOGIC_VECTOR(2 DOWNTO 0); --PUERTO CONECTADO A LAS COLUMNAS DEL TECLADO
+		filas		  : OUT STD_LOGIC_VECTOR(3 DOWNTO 0); --PUERTO CONECTADO A LA FILAS DEL TECLADO
+		
+		
 		--Buttons
+		
+		
 		KEY		: 	IN 		std_logic_vector( 1 DOWNTO 0 )
         );
 	END vga_controllers_acc_keyb;
@@ -63,13 +72,35 @@ ARCHITECTURE vga_controllers_acc_keyb OF vga_controllers_acc_keyb IS
 	END COMPONENT;
 
     SIGNAL acel_input : std_logic_vector (7 downto 0);
+	--01 is down, 10 is up del teclado
+	 
+	 COMPONENT LIB_TECLADO_MATRICIAL_4x4_INTESC_RevA is
+	  GENERIC(
+				FREQ_CLK : INTEGER := 50_000_000         --FRECUENCIA DE LA TARJETA
+		);
 
+			PORT(
+			CLK 		  : IN  STD_LOGIC; 						  --RELOJ FPGA
+			COLUMNAS   : IN  STD_LOGIC_VECTOR(2 DOWNTO 0); --PUERTO CONECTADO A LAS COLUMNAS DEL TECLADO
+			FILAS 	  : OUT STD_LOGIC_VECTOR(3 DOWNTO 0); --PUERTO CONECTADO A LA FILAS DEL TECLADO
+			BOTON_PRES : OUT STD_LOGIC_VECTOR(3 DOWNTO 0); --PUERTO QUE INDICA LA TECLA QUE SE PRESION�
+			IND		  : OUT STD_LOGIC							  --BANDERA QUE INDICA CUANDO SE PRESION� UNA TECLA (S�LO DURA UN CICLO DE RELOJ)
+	   );
+
+	 end component LIB_TECLADO_MATRICIAL_4x4_INTESC_RevA;
+	 
+	 
+	 SIGNAL IND_teclado : STD_LOGIC;
+	 SIGNAL col_teclado : STD_LOGIC_VECTOR (2 downto 0);
+	 SIGNAL BOTON_PRES : STD_LOGIC_VECTOR(3 downto 0);
+	 
+	-----------------------------------------------------------
     SIGNAL output_red : STD_LOGIC_VECTOR(1 DOWNTO 0);
     SIGNAL output_blue : STD_LOGIC_VECTOR(1 DOWNTO 0);
 BEGIN
 	
 	acc_component : acelerometro port map (CLOCK_50, KEY, GSENSOR_INT, GSENSOR_SDI, GSENSOR_SDO, GSENSOR_CS_N, GSENSOR_SCLK, acel_input);
-
+	teclado_pong : LIB_TECLADO_MATRICIAL_4x4_INTESC_RevA generic map (50000000) port map(CLOCK_50, col, filas, BOTON_PRES, IND_teclado);
     process(acel_input)
     begin
         IF (acel_input(7) = '1' or acel_input(6) = '1') THEN
@@ -81,15 +112,31 @@ BEGIN
         END IF;
     end process;
 
-    process(SW)
+--    process(SW)
+--    begin
+--        IF (SW(0) = '1') THEN
+--            output_blue <= "01";
+--        ELSIF (SW(1) = '1') THEN
+--            output_blue <= "10";
+--        ELSE
+--            output_blue <= "00";
+--        END IF;
+--    end process;
+
+    process(CLOCK_50, Boton_pres, IND_teclado)
     begin
-        IF (SW(0) = '1') THEN
+	 if(rising_edge(CLOCK_50)) THEN
+        LEDR(0) <='0';
+			LEDR(1) <='0';
+         output_blue <= "00";
+		  IF ( IND_teclado = '1' and boton_pres=x"2") THEN
             output_blue <= "01";
-        ELSIF (SW(1) = '1') THEN
-            output_blue <= "10";
-        ELSE
-            output_blue <= "00";
+				LEDR(0) <= '1';
+        ELSIF (IND_teclado = '1' and boton_pres=x"8") THEN
+				output_blue <= "10";
+				LEDR(1) <= '1';
         END IF;
+	 END IF;
     end process;
 
     vga_component : vga_pong port map (
